18cf24de7016aa04e88297d6fe5d49be3732901a
Fathimath Shahama A K <fathimathshahama50@gmail.com>
2026-09-23 22:21:57 +0530

    added js

--- Quiz.js
+++ Quiz.js
@@ -1,2 +1,20 @@
 const startScreen = document.getElementById("strtScreen");
 const QuizScreen = document.getElementById("quizScrn");
+const resultScree = document.getElementById("rsltScreen");
+const startButton = document.getElementById("strtBtn");
+const questionText = document.getElementById("qsText");
+const answerContainer = document.getElementById("ansrContainer");
+const currentQsSpan = document.getElementById("currentQs");
+const totalQsSpan = document.getElementById("strtBtn");
+const scoreSpan = document.getElementById("score");
+const finalScoreSpan = document.getElementById("fnlScore");
+const maxScoreSpan = document.getElementById("maxScore");
+const resultMessage = document.getElementById("rsltMsg");
+const restartButton = document.getElementById("restartBtn");
+const progressBar = document.getElementById("progress");
+
+const quizQs={
+    quetions:
+}
+
+
